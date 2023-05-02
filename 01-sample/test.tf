5f3399e129d1cc0d45cdf815c39020c8ee2e7ae6
Rudra <Rudrayyaswamims@gmail.com>
2023-05-02 15:11:23 +0530

    updating learn-terraform

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
-    ami = "ami-0b5a2b5b8f2be4ec2"
-    instance_type = "t3.micro"
+    ami                    = "ami-0b5a2b5b8f2be4ec2"
+    instance_type          = "t3.micro"
     vpc_security_group_ids = ["sg-0d55a1e1e3546f2a0"]
     tags = {
         Name = "test"
