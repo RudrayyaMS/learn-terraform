bef5a5dcac64d8efe2dfd4aa0d9c169e8c98fb32
Rudra <Rudrayyaswamims@gmail.com>
2023-05-02 15:35:54 +0530

    updating learn-terraform

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"   # ami name
-  owners           = [" 973714476881"]            # ami location 1st part
+  owners           = ["973714476881"]            # ami location 1st part
 
 }
 
