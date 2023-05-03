f64685a8c123f6a7d6bb4d31610533cf5260160b
Rudra <Rudrayyaswamims@gmail.com>
2023-05-03 07:07:11 +0530

    updating learn-terraform

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -19,3 +19,28 @@ variable "cli" {}
 output "cli" {
   value = var.cli
 }
+
+variable "sample5" {
+  default = "Hello"
+}
+
+variable "sample6" {
+  default = [
+    "hello",
+    100,
+    true,
+    "world"
+  ]
+}
+
+variable "sample7" {
+  default = {
+    string = "hello"
+    number = 100
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "variable samaple5 - ${var.sample5}, First value in List - ${var.sample6[0]}, Boolean value of Map - ${var.sample7["boolean]"}}
+}
