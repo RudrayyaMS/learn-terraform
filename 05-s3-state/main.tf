004f93be6c61db19bca4162a3ff88c28da941558
Rudra <Rudrayyaswamims@gmail.com>
2023-05-06 07:51:44 +0530

    updating learn-terraform

---
+++ 05-s3-state/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "terraformm-71"
+    key    = "05-s3-state/terraform.tfstate"   # file name
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0b5a2b5b8f2be4ec2"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0d55a1e1e3546f2a0"]
+  tags = {
+    Name = "test"
+  }
+}
