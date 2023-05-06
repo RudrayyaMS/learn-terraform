1941f5580e398d91c92c036fcfce7e8d083efe02
Rudra <Rudrayyaswamims@gmail.com>
2023-05-06 17:25:00 +0530

    updating learn-terraform

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -5,6 +5,7 @@ data "aws_ami" "ami" {
 
 }
 
+ input as map of maps
 resource "aws_instance" "instances" {
   for_each = var.instances
   ami                    = "ami-0b5a2b5b8f2be4ec2"
@@ -27,5 +28,55 @@ variable "instances" {
       }
     }
   }
+
+output "ec2" {
+  value = [for k , v in aws_instance.instances : "$(k) - $(v.public_ip)"]
+}
+#immature code
+#variable "names" {
+#  default = ["catalogue" , "user"]
+#}
+#
+#variable "types" {
+#  default = [ "t3.micro" , "t3.small"]
+#}
+#
+#resource "aws_instance" "instances" {
+#  count  = length(var.names)
+#  ami                    = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type          = var.types[count.index]
+#  vpc_security_group_ids = ["sg-0d55a1e1e3546f2a0"]
+#  tags = {
+#    Name = var.names[count.index]
+#  }
+#}
+
+## Little mature code
+#resource "aws_instance" "instances" {
+#  count    = length(var.instances)
+#  ami                    = "ami-0b5a2b5b8f2be4ec2"
+#  instance_type          = var.demo[count.index]["type"]
+#  vpc_security_group_ids = ["sg-0d55a1e1e3546f2a0"]
+#  tags = {
+#    Name = var.demp[count.index]["name"]
+#  }
+#}
+#
+#variable "demo" {
+#  default = [
+#    {
+#      name ="catalogue"
+#      type ="t3.micro"
+#    },
+#     {
+#      name  = "user"
+#      type = "t3.small"
+#    }
+#  ]
+#}
+
+
+
+
 
 
