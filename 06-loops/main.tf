de57e5b6031ba614cd5250ae7e29b6da0de50431
Rudra <Rudrayyaswamims@gmail.com>
2023-05-06 15:56:24 +0530

    updating learn-terraform

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -5,3 +5,29 @@ variable "d1" {
 resource "null_resource" "nothing" {
   count = var.d1
 }
+#----------------
+variable "d2" {
+  default = [ 0, 1, 2, 3, 4]
+}
+
+resource "null_resource" "d2" {
+  count = length(var.d2)
+}
+#-----------------
+
+variable "d3" {
+  default = {
+    apple = {
+      name = "apple"
+      count =3
+    }
+    banana = {
+      name = "banana"
+      count =30
+    }
+  }
+}
+
+resource "null_resource" "d3" {
+  for_each = var.d3
+}
