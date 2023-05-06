16836d9abe1f506c3fcf11d72372010e8a39e048
Rudra <Rudrayyaswamims@gmail.com>
2023-05-06 19:53:27 +0530

    updating learn-terraform

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -4,3 +4,11 @@ resource "aws_instance" "ec2" {
 }
 
 variable "instance_type" {}
+
+variable "create_instance" {}
+
+resource "aws_instance" "ec2" {
+  count = var.create_instance ? 1 : 0
+  ami = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
+}
