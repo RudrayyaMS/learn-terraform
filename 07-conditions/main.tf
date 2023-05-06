08975b19c3fa4721703f6f5884d878fa988fcaab
Rudra <Rudrayyaswamims@gmail.com>
2023-05-06 20:09:38 +0530

    updating learn-terraform

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -8,7 +8,7 @@ variable "instance_type" {}
 variable "create_instance" {}
 
 resource "aws_instance" "ec21" {
-  count = var.create_instance ? 1 : 0
+  count = tobool(var.create_instance) ? 1 : 0              # convert string to boolean
   ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
 }
