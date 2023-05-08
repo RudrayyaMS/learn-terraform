5d63ef3b0a12cdea2c5b9faa75fd988db35a41d8
Rudra <Rudrayyaswamims@gmail.com>
2023-05-08 19:50:12 +0530

    updating learn-terraform

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -1,7 +1,7 @@
 # DNS record creation
 resource "aws_route53_record" "record" {
   zone_id = "Z0900206KGOHOIP4AMC1"
-  name    = "$(var.component)-dev.devopsm71.online"
+  name    = "${var.component}-dev.devopsm71.online"
   type    = "A"
   ttl     = 30
   records = [var.private_ip]
