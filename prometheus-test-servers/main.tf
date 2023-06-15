4c27412f0ffa9c008ce23c1e90897e54befb2253
Rudra <Rudrayyaswamims@gmail.com>
2023-06-15 18:56:03 +0530

    updating learn-terraform

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,4 +1,4 @@
-resource "aws_spot_instance_request" "node1" {
+resource "aws_instance" "node1" {
   ami           = "ami-008ff397e44313f05"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0d55a1e1e3546f2a0"]
@@ -8,14 +8,14 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
-resource "aws_ec2_tag" "node1" {
-  resource_id = aws_spot_instance_request.node1.spot_instance_id
-  key         = "Name"
-  value       = "prom-test-server"
-}
+#resource "aws_ec2_tag" "node1" {
+#  resource_id = aws_instance.node1.spot_instance_id
+#  key         = "Name"
+#  value       = "prom-test-server"
+#}
 
 
-resource "aws_spot_instance_request" "node2" {
+resource "aws_instance" "node2" {
   ami           = "ami-008ff397e44313f05"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0d55a1e1e3546f2a0"]
@@ -25,8 +25,8 @@ resource "aws_spot_instance_request" "node2" {
   }
 }
 
-resource "aws_ec2_tag" "node2" {
-  resource_id = aws_spot_instance_request.node2.spot_instance_id
-  key         = "Name"
-  value       = "prom-test-node"
-}
+#resource "aws_ec2_tag" "node2" {
+#  resource_id = aws_instance.node2.spot_instance_id
+#  key         = "Name"
+#  value       = "prom-test-node"
+#}
