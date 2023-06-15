6ee3bba927e3d1afa21cdfae297cee317de741f8
Rudra <Rudrayyaswamims@gmail.com>
2023-06-15 16:25:39 +0530

    updating learn-terraform

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -8,6 +8,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-008ff397e44313f05"
@@ -17,4 +23,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
